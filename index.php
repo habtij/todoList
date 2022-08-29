cac9b64204a10584131c6813fa303b92fd11c83f
Habeeb <tijanihabeeb12@outlook.com>
2022-08-29 06:23:58 -0400

    Include app.css to index.php and update the changes of index.php

--- index.php
+++ index.php
@@ -12,26 +12,36 @@ if (file_exists("todo.json")) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="app.css">
 </head>
 <body>
-    <form action="newtodo.php" method="post">
-        <input type="text" name="todo_name" placeholder="Enter you todo">
-        <button>New Todo</button>
-    </form>
-    <br />
-    <?php foreach($todos as $todoName => $todo):?>
-        <div style='margin-bottom: 20px'>
-            <form style="display: inline-block" action="change_status.php" method="post">
-                <input type="hidden" name="todo_name" value="<?php echo $todoName ?>" />
-                <input type="checkbox" <?php echo $todo['completed'] ? 'checked' : ''; ?> />
-            </form>
-            <?php echo $todoName; ?>
-            <form style="display: inline-block" action="delete.php" method="post">
-                <input type="hidden" name="todo_name" value="<?php echo $todoName; ?>">
-                <button>Delete</button>
-            </form>
-        </div>
-    <?php endforeach; ?>
+    <div class="container">
+        <h1 class="text-center">TODO APPLICATION</h1>
+        <form action="newtodo.php" method="post">
+            <input class="form-control" type="text" name="todo_name" placeholder="Enter you todo">
+            <button class="btn">New Todo</button>
+        </form>
+        <h2 class="text-center border-topbottom">TODO(S)</h2>
+        <table>
+        <?php foreach($todos as $todoName => $todo):?>
+            <tr>
+                <td>
+                    <form style="display: inline-block" action="change_status.php" method="post">
+                        <input type="hidden" name="todo_name" value="<?php echo $todoName ?>" />
+                        <input type="checkbox" <?php echo $todo['completed'] ? 'checked' : ''; ?> />
+                    </form>
+                </td>
+                <td><?php echo $todoName; ?></td>
+                <td>
+                    <form style="display: inline-block" action="delete.php" method="post">
+                        <input type="hidden" name="todo_name" value="<?php echo $todoName; ?>">
+                        <button class="btn">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+        </table>
+    </div>
 
     <script>
         const checkboxes = document.querySelectorAll('input[type=checkbox]');
